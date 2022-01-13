61e23cc57f4d02b11733164a12f3e9c997adb340
Sandra Calero <3358@holbertonschool.com>
2022-01-13 02:11:59 -0500

    fix sudo symlink

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,9 +4,6 @@
 sudo apt-get -y update
 sudo apt-get -y install nginx
 
-# Give ownership
-sudo chown -R ubuntu:ubuntu /data/
-
 # Create directories
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
@@ -21,7 +18,10 @@ echo "<html>
 </html>" > /data/web_static/releases/test/index.html
 
 # Create a symbolic link
-ln -fs /data/web_static/releases/test/ /data/web_static/current
+sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership
+sudo chown -R ubuntu:ubuntu /data/
 
 # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
 file="/etc/nginx/sites-available/default"
