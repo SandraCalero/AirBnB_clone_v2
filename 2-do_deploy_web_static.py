dc0bddba5e51b93207bbddb15b4ccb986d10e398
Sandra Calero <3358@holbertonschool.com>
2022-01-13 17:41:05 -0500

    add a Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -24,16 +24,16 @@ def do_deploy(archive_path):
 
     try:
         put(archive_path, "/tmp/")
-        run("sudo mkdir -p /data/web_static/releases/{}/".format(filename_woe))
-        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+        run("mkdir -p /data/web_static/releases/{}/".format(filename_woe))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
             .format(filename_we, filename_woe))
-        run("sudo rm -rf /tmp/{}".format(filename_we))
-        run("sudo mv /data/web_static/releases/{}/web_static/* /data/\
+        run("rm -rf /tmp/{}".format(filename_we))
+        run("mv /data/web_static/releases/{}/web_static/* /data/\
             web_static/releases/{}/".format(filename_woe, filename_woe))
-        run("sudo rm -rf /data/web_static/releases/{}/web_static"
+        run("rm -rf /data/web_static/releases/{}/web_static"
             .format(filename_woe))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
             .format(filename_woe))
         return True
     except Exception:
