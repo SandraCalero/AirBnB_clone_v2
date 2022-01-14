26fef931f2c0a8bd87effd761853ccffb190298e
Sandra Calero <3358@holbertonschool.com>
2022-01-13 19:50:45 -0500

    add a Fabric script (based on the file 2-do_deploy_web_static.py) that creates and distributes an archive to your web servers, using the function deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,6 +2,7 @@
 """Fabric script that creates and distributes an archive
 to your web servers, using the function deploy
 """
+from genericpath import isdir
 from os.path import exists
 from fabric.api import local, put, run, env
 from datetime import datetime
@@ -13,13 +14,15 @@ def do_pack():
     """Fabric script that generates a .tgz archive from the contents of the
     web_static folder of your AirBnB Clone repo, using the function do_pack
     """
-    local("mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    tgz_name = "web_static_" + date + ".tgz"
-    archive_path = local("tar -cvzf versions/{} web_static".format(tgz_name))
-    if archive_path.failed:
+    try:
+        if isdir('versions') is False:
+            local("mkdir -p versions")
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        tgz_name = "web_static_" + date + ".tgz"
+        local("tar -cvzf versions/{} web_static".format(tgz_name))
+        return "versions/{}".format(tgz_name)
+    except Exception:
         return None
-    return archive_path
 
 
 def do_deploy(archive_path):
@@ -29,20 +32,19 @@ def do_deploy(archive_path):
     if exists(archive_path) is False:
         return False
 
-    start = archive_path.find("web_static")
-    end = archive_path.find(".tgz")
-    # Getting filename withouth extension
-    filename_woe = archive_path[start:end]
-    # Getting filename with extension
-    filename_we = archive_path[start:]
-
     try:
+        start = archive_path.find("web_static")
+        end = archive_path.find(".tgz")
+        # Getting filename withouth extension
+        filename_woe = archive_path[start:end]
+        # Getting filename with extension
+        filename_we = archive_path[start:]
         put(archive_path, "/tmp/")
         run("mkdir -p /data/web_static/releases/{}/".format(filename_woe))
-        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
             .format(filename_we, filename_woe))
         run("rm /tmp/{}".format(filename_we))
-        run("sudo mv /data/web_static/releases/{}/web_static/* /data/web_sta\
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_sta\
 tic/releases/{}/".format(filename_woe, filename_woe))
         run("rm -rf /data/web_static/releases/{}/web_static"
             .format(filename_woe))
