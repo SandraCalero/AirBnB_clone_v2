a01ecd0b49a70d874243a2f783eee85684ef34c8
Sandra Calero <3358@holbertonschool.com>
2022-01-29 16:00:20 -0500

    change name of teardown

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,6 +7,13 @@ from models.state import State
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def teardown_db():
+    """Remove the current SQLAlchemy Session
+    """
+    storage.close()
+
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Render the list of the states
@@ -15,12 +22,5 @@ def states_list():
     return render_template('7-states_list.html', states=states)
 
 
-@app.teardown_appcontext
-def close_session():
-    """Remove the current SQLAlchemy Session
-    """
-    storage.close()
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
