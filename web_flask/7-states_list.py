794568f1cc51449c7c3ee0e1e2becb8b60ed721d
Sandra Calero <3358@holbertonschool.com>
2022-01-29 19:01:48 -0500

    add spaces

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,12 +3,8 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.jinja_env.trim_blocks = True
-app.jinja_env.lstrip_blocks = True
 
 
 @app.route("/states_list")
